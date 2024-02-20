b12fbc0a458eea827ff521fa186a8b06bd0ffec8
vaishnavi8446 <vaishnaviambolkar8446@gmail.com>
2024-02-20 12:31:55 +0530

    updated upload apis by separating db files

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,23 +1,20 @@
-const db = require("../db/db");
+const conn = require("../db/db");
+const { addImage, fetchImage, getImageById } = require("../db/uploadDB");
 
 exports.uploadImage = async (req, res) => {
   try {
-    let sql = "INSERT INTO  images (name,type,size) VALUES (?,?,?)";
-
     const { originalname, mimetype, size } = req.file;
-  
-    db.query(sql, [originalname, mimetype, size], function (error, result) {
-      if (error) {
-        console.error(error);
-        return res
-          .status(500)
-          .send({ status_code: 500, message: "Failed to upload recipe image" });
-      }
-      return res.status(200).send({
-        status_code: 200,
-        message: "Successfully uploaded!",
-        data: result,
-      });
+    const uploadRes = await addImage(conn, originalname, mimetype, size);
+
+    if (!uploadRes) {
+      return res
+        .status(500)
+        .send({ status_code: 500, message: "Failed to upload recipe image" });
+    }
+    return res.status(200).send({
+      status_code: 200,
+      message: "Successfully uploaded!",
+      data: uploadRes,
     });
   } catch (error) {
     console.error(error);
@@ -28,21 +25,36 @@ exports.uploadImage = async (req, res) => {
 exports.getImage = async (req, res) => {
   try {
     const filename = req.params.filename;
-    //  res.sendFile(`${__dirname}/../uploads/${filename}`);
-    let sql = "SELECT * from images WHERE name = ? ";
+    const fetchImageRes = await fetchImage(conn, filename);
+    if (!fetchImageRes) {
+      return res
+        .status(500)
+        .send({ status_code: 500, message: "Failed to fetch recipe image" });
+    }
+    return res.status(200).send({
+      status_code: 200,
+      message: "Successfully fetched all the images!",
+      data: fetchImageRes,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal server error" });
+  }
+};
 
-    db.query(sql, [filename], function (error, result) {
-      if (error) {
-        console.error(error);
-        return res
-          .status(500)
-          .send({ status_code: 500, message: "Failed to fetch recipe image" });
-      }
-      return res.status(200).send({
-        status_code: 200,
-        message: "Successfully fetched image!",
-        data: result,
-      });
+exports.getImageById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const fetchImageRes = await getImageById(conn, id);
+    if (!fetchImageRes) {
+      return res
+        .status(500)
+        .send({ status_code: 500, message: "Failed to fetch recipe image" });
+    }
+    return res.status(200).send({
+      status_code: 200,
+      message: "Successfully fetched image!",
+      data: fetchImageRes,
     });
   } catch (error) {
     console.error(error);

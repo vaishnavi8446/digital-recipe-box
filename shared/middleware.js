8b709c82b2ec9cd8847b47d427a58a1ae63cc29c
vaishnavi8446 <vaishnaviambolkar8446@gmail.com>
2024-03-02 21:24:43 +0530

    removed unnecessary code

--- shared/middleware.js
+++ shared/middleware.js
@@ -1,30 +1,5 @@
-// const jwt = require("jsonwebtoken");
-// const secretKey = "your-secret-key";
-
-// const verifyToken = (req, res, next) => {
-//   const token = req.headers.authorization;
-
-//   if (!token) {
-//     return res
-//       .status(401)
-//       .json({ message: "Authorization token not provided" });
-//   }
-
-//   jwt.verify(token, secretKey, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).json({ message: "Invalid token" });
-//     }
-//     req.user = decoded;
-//     next();
-//   });
-// };
-
-// module.exports = {
-//   verifyToken,
-// };
 const jwt = require("jsonwebtoken");
-const secretKey = "your-secret-key"; // This should be a secret key used to sign your JWT tokens
-
+const secretKey = "your-secret-key";
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -34,10 +9,9 @@ const verifyToken = (req, res, next) => {
       .json({ message: "Authorization token not provided" });
   }
 
-  // Extract the token from the Authorization header (Bearer token)
   const tokenParts = token.split(" ");
-  const tokenType = tokenParts[0]; // Bearer
-  const authToken = tokenParts[1]; // Token value
+  const tokenType = tokenParts[0];
+  const authToken = tokenParts[1];
 
   if (tokenType !== "Bearer" || !authToken) {
     return res
@@ -45,13 +19,12 @@ const verifyToken = (req, res, next) => {
       .json({ message: "Invalid authorization token format" });
   }
 
-  // Verify the token using the secret key
   jwt.verify(authToken, secretKey, (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: "Invalid token" });
     }
-    req.user = decoded; // Attach the decoded user information to the request object
-    next(); // Call the next middleware function
+    req.user = decoded;
+    next();
   });
 };
 
